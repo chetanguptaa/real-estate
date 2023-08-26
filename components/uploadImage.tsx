1d8097375be3dff65969a68271c654f6a39ee28e
chetan gupta <chetangupta123raj@gmail.com>
2023-08-26 18:34:35 +0530

    updated uploadImage.ts

--- components/uploadImage.tsx
+++ components/uploadImage.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Label } from './ui/label'
 import { Input } from './ui/input'
+import { useRecoilState } from 'recoil'
+import { imageState } from '@/store/uploadImageAtom'
+import { createAListingAtom } from '@/store/createAListingAtom';
 
-const uploadImage = () => {
-  return (
-    <div>
-        <Label htmlFor="picture">Picture</Label>
-        <Input id="picture" type="file" />
-    </div>
-  )
+const UploadImage = () => {
+    const [listing, setListing] = useRecoilState(createAListingAtom);
+    function onImageChange(e: any) {
+        // setImages([...e.target.files]);
+        setListing(listing.image([...e.target.files]));
+    }
+    return (
+        <div>
+            <Label htmlFor="picture">Picture</Label>
+            <Input id="picture" type="file" multiple accept='images/*' onChange={onImageChange}/>
+        </div>
+    )
 }
 
-export default uploadImage
+export default UploadImage
