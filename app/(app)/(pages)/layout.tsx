b06fe97531aff6e3e425ef5fab6290c5f272f227
chetan gupta <chetangupta123raj@gmail.com>
2023-08-26 18:06:34 +0530

    updated layouts

--- app/(app)/(pages)/layout.tsx
+++ app/(app)/(pages)/layout.tsx
@@ -1,13 +1,5 @@
-'use client'
-
-import {
-  RecoilRoot,
-} from 'recoil';
-
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <RecoilRoot>
       <main className="h-full">{children}</main>
-    </RecoilRoot>
   )
 }
