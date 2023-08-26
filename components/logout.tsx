ed9b73e24bde69c3cffc8d9af5932f805da32e48
chetan gupta <chetangupta123raj@gmail.com>
2023-08-26 14:06:06 +0530

    updated logout.tsx

--- components/logout.tsx
+++ components/logout.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import axios from "axios";
 import { useRouter } from "next/navigation";
 import { DropdownMenuItem } from "./ui/dropdown-menu";
 import { LogOut } from "lucide-react";
