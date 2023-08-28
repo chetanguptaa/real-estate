cfde6809ddd2df487b3597059ffb2a4a09af21b9
chetan gupta <chetangupta123raj@gmail.com>
2023-08-28 19:01:48 +0530

    migrations

---
+++ prisma/migrations/20230827130833_/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Post" ALTER COLUMN "id" SET DEFAULT concat('pos_', replace(cast(gen_random_uuid() as text), '-', ''));
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "id" SET DEFAULT concat('usr_', replace(cast(gen_random_uuid() as text), '-', ''));
